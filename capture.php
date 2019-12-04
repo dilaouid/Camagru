3af1eee3307ae15cb991e91afcd525552cb02a14
dilaouid <dilaouid@github.fr>
2019-12-04 09:12:13 +0100

    update

--- capture.php
+++ capture.php
@@ -36,7 +36,7 @@ if (isset($_POST['x_pos'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?= $global['sitename'] ?> | Capture</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/css/navbar.css">
     <link rel="stylesheet" href="assets/css/footer.css">
